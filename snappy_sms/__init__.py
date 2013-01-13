2661ccfa471e860b57aba5398fdef42887981ae8
Adrián Pérez Orozco <krun@windows>
2013-01-13 01:35:43 +0100

    Forcing TLSv1

--- snappy_sms/__init__.py
+++ snappy_sms/__init__.py
@@ -1,4 +1,4 @@
-import urllib, httplib
+import urllib, httplib, socket, ssl
 
 MOVISTAR_USERNAME = 600000000
 MOVISTAR_PASSWORD = 'tu_clave_de_la_api_sms'
@@ -13,6 +13,9 @@ def send_sms(to, text, username=MOVISTAR_USERNAME, password=MOVISTAR_PASSWORD):
   data = urllib.urlencode(form)
 
   conn = httplib.HTTPSConnection("opensms.movistar.es")
+  #This is to force TLSv1. Connection might fail otherwise.
+  sock = socket.create_connection((conn.host, conn.port), conn.timeout, conn.source_address)
+  conn.sock = ssl.wrap_socket(sock, conn.key_file, conn.cert_file, ssl_version=ssl.PROTOCOL_TLSv1)
   conn.request("POST", "/aplicacionpost/loginEnvio.jsp", data, headers)
   resp = conn.getresponse()
   rred = resp.read()
